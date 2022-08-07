0ec6bdf0e5d87300a3e40f4949e3354c0830a057
unknown <padinmichael201@gmail.com>
2022-08-08 05:14:59 +0800

    added a wrapper div

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,9 +5,7 @@ import { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 
-import { GoogleOAuthProvider } from '@react-oauth/google';
-
-
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const [isSSR, setIsSSR] = useState(true);
@@ -18,18 +16,22 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
 
   if (isSSR) return null;
   return (
-    <GoogleOAuthProvider clientId = {`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}>
-    <div>
-      <Navbar />
-      <div className="flex gap-6 md:gap-20">
-        <div className="h-[92vh] overflow-hidden xl:hover-auto">
-          <Sidebar />
-        </div>
-        <div className="mt-4 flex flex-col gap-10 overflow-auto min-h-full videos flex-1">
-          <Component {...pageProps} />
+    <GoogleOAuthProvider
+      clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}
+    >
+      <div className = "overflow-hidden h-[100vh]">
+        <Navbar />
+        <div className="xl:max-w-[1150px] mx-auto mt-[60px] ">
+          <div className="flex gap-6 md:gap-20">
+            <div className="h-[92vh] overflow-hidden xl:hover-auto">
+              <Sidebar />
+            </div>
+            <div className="mt-4 flex flex-col gap-10 overflow-auto min-h-full videos flex-1">
+              <Component {...pageProps} />
+            </div>
+          </div>
         </div>
       </div>
-    </div>
     </GoogleOAuthProvider>
   );
 };
