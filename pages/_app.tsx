3f6dc18307c4fbcaf6ee13bff4fb45d8317c3bff
Your Name <padinmichael201@gmail.com>
2022-08-12 23:06:53 +0800

    Removed unnecessary style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 
 import { GoogleOAuthProvider } from "@react-oauth/google";
-
+const SidebarMemo = React.memo(Sidebar);
+const NavbarMemo = React.memo(Navbar);
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const [isSSR, setIsSSR] = useState(true);
 
@@ -19,14 +20,14 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
     <GoogleOAuthProvider
       clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}
     >
-      <div className = "h-[100vh]">
-        <Navbar />
+      <div>
+        <NavbarMemo />
         <div className="xl:max-w-[1150px] mx-auto mt-[60px] ">
           <div className="flex gap-6 md:gap-20">
-            <div className="relative w-[356px]">
-              <Sidebar />
+            <div className="relative lg:w-[356px] pt-5">
+              <SidebarMemo />
             </div>
-            <div className="mt-4 flex flex-col gap-10 overflow-auto min-h-full videos flex-1">
+            <div className=" pt-5 flex flex-col gap-10 overflow-auto min-h-full videos flex-1">
               <Component {...pageProps} />
             </div>
           </div>
