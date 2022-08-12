2982d97b5c1a91e9cb34e80de86622f5d1fe2884
Your Name <padinmichael201@gmail.com>
2022-08-12 23:02:06 +0800

    code formatted

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -5,29 +5,27 @@ import { useRouter } from "next/router";
 import { topics } from "../utils/constants";
 
 const Discover = () => {
-  
-    const router = useRouter();
-    const {topic} = router.query;
+  const router = useRouter();
+  const { topic } = router.query;
 
-    
-    // topic styles 
-    const activeTopicStyle =
-    "xl:border-2 hover:bg-secondary xl:border-[#fe2c55] px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-[#fe2c55]";
-    const topicStyle =
-    "xl:border-2 hover:bg-secondary hover:border-[#fe2c55]  xl:border-gray-300 px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-black";
+  // topic styles
+  const activeTopicStyle =
+    "xl:border-[1px] hover:bg-secondary xl:border-[#fe2c55] px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-[#fe2c55]";
+  const topicStyle =
+    "xl:border-[1px] hover:bg-secondary hover:border-[#fe2c55]  xl:border-gray-300 px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-gray-500";
 
   return (
-    <div className="xl:border-b-2 xl:border-gray-200 pb-6">
+    <div className="border-b-[1px] xl:border-gray-200 pb-6">
       <p className="text-gray-500 font-semibold m-3 mt-4 hidden xl:block">
         Popular Topics
       </p>
       <div className="flex gap-3 flex-wrap">
         {topics.map((item) => (
           <Link href={`/?topic=${item.name}`} key={item.name}>
-            <div className={topic === item.name ? activeTopicStyle :topicStyle}>
-              <span className="font-bold text-2xl xl-text-md">
-                {item.icon}
-              </span>
+            <div
+              className={topic === item.name ? activeTopicStyle : topicStyle}
+            >
+              <span className="font-bold text-2xl xl-text-md">{item.icon}</span>
               <span className="font-medium text-md hidden xl:block capitalize">
                 {item.name}
               </span>
